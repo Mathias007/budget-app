e84a00e89d86d044ec64808c9ac49d9878d732e9
Mathias007 <mathias.najlepszy@gmail.com>
2021-01-26 21:49:04 +0100

    Implement Theme provider in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
-import "App.css";
+import GlobalStyle from "./index.css";
+
+import theme from "utils/theme";
 
 import { Navigation } from "components";
 
+
 function App() {
     return (
-        <div className="App">
-            <Navigation />
-        </div>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <div className="App">
+                <Navigation items={[]} />
+            </div>
+        </ThemeProvider>
     );
 }
 
