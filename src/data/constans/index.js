4c3d625629f8d45ff1098baa33341939d31d1c96
Mathias007 <mathias.najlepszy@gmail.com>
2021-01-29 19:35:07 +0100

    Create budget reducer & budget constans with GET types.

---
+++ src/data/constans/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-anonymous-default-export */
+export * from "./budget.constans";
+
+// LOADING STATES
+export const LOADING_STATES = {
+    INITIAL: "INITIAL",
+    LOADING: "LOADING",
+    LOADED: "LOADED",
+    FAILER: "FAILED",
+};
+
+export default {
+    LOADING_STATES,
+};
