5f64b21bca5c0cc4af6aa745b7d5d95d6c3c401d
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-03 18:35:04 +0100

    Count categoryLeftValue & create CategoryAmount.

--- src/pages/Budget/components/BudgetCategoryList/ParentCategory.js
+++ src/pages/Budget/components/BudgetCategoryList/ParentCategory.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useMemo } from "react";
 
-import { ParentCategory as Root } from "./BudgetCategoryList.css";
+import {
+    ParentCategory as Root,
+    CategoryAmount,
+} from "./BudgetCategoryList.css";
 
-function ParentCategory({ name, onClick }) {
-    return <Root onClick={onClick}>{name}</Root>;
+function ParentCategory({ name, onClick, categories, transactions }) {
+    const categoryLeftValue = useMemo(() => {
+        const budgeted = (() => {
+            try {
+                return categories.reduce(
+                    (acc, category) => acc + category.budget,
+                    0
+                );
+            } catch (error) {
+                return null;
+            }
+        })();
+
+        const parentCategoryTransactions = transactions.filter(
+            (transaction) => {
+                return categories.find(
+                    (category) => category.categoryId === transaction.categoryId
+                );
+            }
+        );
+
+        const spentOnParentCategory = parentCategoryTransactions.reduce(
+            (acc, transaction) => acc + transaction.amount,
+            0
+        );
+
+        const totalLeft = budgeted ? budgeted - spentOnParentCategory : null;
+
+        return totalLeft;
+    }, [categories, transactions]);
+
+    return (
+        <Root onClick={onClick}>
+            <span>{name}</span>
+            <CategoryAmount negative={categoryLeftValue < 0}>
+                {categoryLeftValue}
+            </CategoryAmount>
+        </Root>
+    );
 }
 
 export default ParentCategory;
