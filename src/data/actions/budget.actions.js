945a3f4476fa724481a7dd8004fadfe750ecd25a
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-06 22:14:51 +0100

    Create notifications middleware.

--- src/data/actions/budget.actions.js
+++ src/data/actions/budget.actions.js
@@ -38,5 +38,6 @@ export const addTransaction = ({ budgetId, data }) => {
     return {
         type: BUDGET_TRANSACTION_ADD,
         promise,
+        successMessage: "Transaction has been added!",
     };
 };
